dc0b58d7df396f03685c5da6ff25fb8ceede5faf
cuiyy <cuiyy@linkcld.com>
2023-11-06 17:29:50 +0800

    fix:根据文字插入图片

--- springTest/src/main/java/linkUtilTest/SignDoc.java
+++ springTest/src/main/java/linkUtilTest/SignDoc.java
@@ -3,6 +3,7 @@ package linkUtilTest;
 import com.example.demo.util.SignDocUtil;
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
+import com.spire.doc.documents.TextSelection;
 
 /**
  * <p>Description: </p>
@@ -14,6 +15,10 @@ import com.spire.doc.FileFormat;
 public class SignDoc {
 
     public static void main(String[] args) throws Exception{
+        test1();
+    }
+
+    public static void test1() throws Exception{
         //添加图片
         String word1 = "http://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/tcsp/docx/1698630863769.docx?Expires=2013990857&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=oaYhD%2B1NOekuYF%2BNA0HdAZAKWsk%3D";
         String word2 = "http://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/tcsp/docx/1698650295402.docx?Expires=2014010290&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=0wyjoLNU3GRYjrWmMx6jyjH2ZWE%3D";
@@ -21,6 +26,11 @@ public class SignDoc {
         Document doc = SignDocUtil.signPic(word1, image, 130, 370, 30, 70);
         //添加文字
         doc = SignDocUtil.signText(doc, "这是一段测试描述", 90, 680, 200, 30);
+
+        TextSelection[] textSelections = new TextSelection[2];
+        textSelections[0].getAsOneRange().getCharacterFormat();
+
+
         // 保存文档
         doc.saveToFile("/Users/cuiyueyang/Desktop/送达回证-sign.docx", FileFormat.Docx);
     }
--- springTest/src/main/java/linkUtilTest/SignPdf.java
+++ springTest/src/main/java/linkUtilTest/SignPdf.java
@@ -14,7 +14,33 @@ import java.util.Calendar;
  **/
 public class SignPdf {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        //根据经纬度
+        test1();
+        //根据文字确定点位
+        test2();
+    }
+
+    public static void test1() throws Exception{
+        try {
+            Calendar cal = Calendar.getInstance();
+            // 当前年
+            int year = cal.get(Calendar.YEAR);
+            String word1 = "http://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/tcsp/docx/1698630863769.docx?Expires=2013990857&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=oaYhD%2B1NOekuYF%2BNA0HdAZAKWsk%3D";
+            String pdf1 = "https://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/aila/79601698375654960.pdf?Expires=2013735655&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=kXJ9YnASuFeqlvRgBjpa6WIImEs%3D";
+            String img1 = "http://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/aila/4951698375938540.jpeg?Expires=2013735938&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=%2FBw41Wde4A98Q%2FGEaKzdA1v%2FFOQ%3D";
+            String img2 = "http://linkcld-tcsp.oss-cn-hangzhou.aliyuncs.com/tcsp/jpg/1698630915112.jpg?Expires=2013990906&OSSAccessKeyId=LTAI5tJZijZ5wB2PDNGthdV7&Signature=pVXAMefbkPpjp4RqX%2BBKSwcYWl0%3D";
+            byte[] bytes = SignPdfUtil.signCoordinate(pdf1, img2, 170, 350, 50, 110, 90, 1);
+            bytes = SignPdfUtil.signAddText(bytes, year + "", 1, 432, 376, "授权委托代理人不放弃陈述、申辩、听证的权力");
+            FileOutputStream pdfOutputStream = new FileOutputStream("/Users/cuiyueyang/Desktop/送达回证-sign.pdf");
+            pdfOutputStream.write(bytes);
+            pdfOutputStream.close();
+        } catch (Exception e) {
+            System.out.println("error:"+e.getMessage());
+        }
+    }
+
+    public static void test2() throws Exception{
         try {
             Calendar cal = Calendar.getInstance();
             // 当前年
